7a79bdf002cd7ba26b974c3b64f733137236233d
Max Broekhuis (LT) <max@maxbenlily.com>
2019-04-23 09:58:39 -0500

    Stated addition of races and base classes

--- Character.h
+++ Character.h
@@ -1,11 +1,20 @@
 #ifndef CHARACTER_H
 #define CHARACTER_H
 
+#include <string.h>
 
 class Character
 {
 public:
     Character();
+
+protected:
+    double health;
+    int level;
+    int experience;
+    double baseDamage;
+    string race;
+    string weapon;
 };
 
 #endif // CHARACTER_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,46 @@
 #include <iostream>
-
+#include "Player.h"
+#include "NPC.h"
+#include "Enemy.h"
 using namespace std;
+
+int calculateDamage(Character &offensive) {
+    double base = offensive.getBaseDamage();
+    string race = offensive.getRace();
+    double raceModifier;
+
+    switch (race) {
+        case "human":
+            raceModifier = 1;
+            break;
+        case "dwarf":
+            raceModifier = 0.85;
+            break;
+        case "elf":
+            raceModifier = 1.15;
+            break;
+        case "tiefling":
+            raceModifier = 1;
+            break;
+        case "halfling":
+            raceModifier = 0.65;
+            break;
+        case "dragonborn":
+            raceModifier = 0.95;
+            break;
+        case "gnome":
+            raceModifier = 0.75;
+            break;
+        case "half-elf":
+            raceModifier = 1;
+            break;
+        case "half-orc":
+            raceModifier = 1.5;
+            break;
+    }
+}
+
+
 
 int main()
 {
